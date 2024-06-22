6ca057ce801864ce88c619061e1663a888b33e13
Albert Xie <albertxie@berkeley.edu>
2024-06-22 11:51:40 -0700

    create ios app

---
+++ FitBotiOS/FitBotiOS/FitBotiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitBotiOSApp.swift
+//  FitBotiOS
+//
+//  Created by Albert Xie on 6/22/24.
+//
+
+import SwiftUI
+
+@main
+struct FitBotiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
